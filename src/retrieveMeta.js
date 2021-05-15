a25cd6b501532530f96cc66f3064aa6c46c50762
skysan <26142873+skysan87@users.noreply.github.com>
2021-05-15 22:53:20 +0900

    refactoring

--- src/retrieveMeta.js
+++ src/retrieveMeta.js
@@ -1,8 +1,6 @@
 const jsforce = require('jsforce')
-const SObjectDao = require('./dao/SObjectDao')
-const DB = require('./dao/DB')
-const SObjectField = require('./entity/SObjectField')
 const config = require('config')
+const SaveMetadataTask = require('./task/SaveMetadataTask')
 
 const SANDBOX_CON = { loginUrl: 'https://test.salesforce.com' }
 
@@ -19,51 +17,15 @@ async function main(sandbox = false) {
     : new jsforce.Connection()
 
   try {
-    // Refresh DB
-    DB.init()
-    await SObjectDao.drop()
-    await SObjectDao.createTableIfNotExists()
-
     // Get Metadata
     await conn.login(USER, PASSWORD + SECURITY_TOKEN)
 
     // NOTE: care about "Metadata limits for the Metadata API"
-    const metadata = await conn.metadata.read('CustomObject', 'Account')
+    const metadatas = await conn.metadata.read('CustomObject', ['Account'])
 
-    const insertData = []
-
-    metadata.fields.forEach(async (f) => {
-      const field = new SObjectField({
-        object_api: metadata.fullName,
-        object_label: metadata.label,
-        field_api: f.fullName,
-        field_label: f.label,
-        field_type: f.type,
-      })
-
-      // picklist
-      if (f.hasOwnProperty('valueSet')) {
-        field.valueset = f.valueSet.valueSetDefinition.value
-          .map((list) => list.label)
-          .join(';')
-      }
-
-      // lookup
-      if (f.type.toLowerCase() === 'lookup') {
-        field.field_type = `${f.type}(${f.referenceTo})`
-      }
-
-      // formula
-      if (f.hasOwnProperty('formula')) {
-        field.field_type = `formula(${f.type})`
-        field.formula = f.formula
-      }
-
-      insertData.push(field)
+    metadatas.forEach(async (meta) => {
+      await SaveMetadataTask.run(meta)
     })
-
-    // save to db
-    await SObjectDao.save(insertData)
 
   } catch (error) {
     console.error(error)
---
+++ src/task/SaveMetadataTask.js
@@ -0,0 +1,51 @@
+const SObjectDao = require('../dao/SObjectDao')
+const DB = require('../dao/DB')
+const SObjectField = require('../entity/SObjectField')
+
+class SaveMetadataTask {
+
+  static async run (metadata) {
+
+    // Refresh DB
+    DB.init()
+    await SObjectDao.drop()
+    await SObjectDao.createTableIfNotExists()
+
+    const insertData = []
+
+    metadata.fields.forEach(async (f) => {
+      const field = new SObjectField({
+        object_api: metadata.fullName,
+        object_label: metadata.label,
+        field_api: f.fullName,
+        field_label: f.label,
+        field_type: f.type,
+      })
+
+      // picklist
+      if (f.hasOwnProperty('valueSet')) {
+        field.valueset = f.valueSet.valueSetDefinition.value
+          .map((list) => list.label)
+          .join(';')
+      }
+
+      // lookup
+      if (typeof f.type === 'String' && f.type.toLowerCase() === 'lookup') {
+        field.field_type = `${f.type}(${f.referenceTo})`
+      }
+
+      // formula
+      if (f.hasOwnProperty('formula')) {
+        field.field_type = `formula(${f.type})`
+        field.formula = f.formula
+      }
+
+      insertData.push(field)
+    })
+
+    // save to db
+    await SObjectDao.save(insertData)
+  }
+}
+
+module.exports = SaveMetadataTask
